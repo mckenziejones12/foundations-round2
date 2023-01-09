de047ffbe605142afa571927987eb2a683dbcbb5
McKenzie Jones <mckenzbeck12@gmail.com>
2023-01-08 19:03:02 -0700

    Create play game functions

--- rps-project/script.js
+++ rps-project/script.js
@@ -1,8 +1,4 @@
-let playerSelection = prompt("Rock, Paper, or Scissors?");
-let lowerCasePlayerSelection = playerSelection.toLowerCase();
-let computerSelection = getComputerChoice();
-
-//random choice selection from computer
+// random choice selection from computer
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomWholeNumber = Math.floor(Math.random() * choices.length);
@@ -10,29 +6,75 @@ function getComputerChoice() {
   return randomChoice;
 }
 
-//play interactive round with player and computer
+// participant variables
+const USER = "Player wins!";
+const COMPUTER = "Computer wins!";
+const TIE = "Tie!";
+
+// play interactive round with player and computer
 function playRound(player, computer) {
   if (player === "rock" && computer === "paper") {
-    return "Computer wins! Paper beats rock!";
+    return COMPUTER;
   }
   if (player === "rock" && computer === "scissors") {
-    return "You win! Rock beats scissors!";
+    return USER;
   }
   if (player === "paper" && computer === "scissors") {
-    return "Computer wins! Scissors beat paper!";
+    return COMPUTER;
   }
   if (player === "paper" && computer === "rock") {
-    return "You win! Paper beats rock!";
+    return USER;
   }
   if (player === "scissors" && computer === "rock") {
-    return "Computer wins! Rock beats scissors!";
+    return COMPUTER;
   }
   if (player === "scissors" && computer === "paper") {
-    return "You win! Scissors beat paper!";
+    return USER;
   }
-  return "You tie!";
+  return TIE;
 }
 
-console.log("round: ", playRound(lowerCasePlayerSelection, computerSelection));
-console.log("player selection: ", lowerCasePlayerSelection);
-console.log("computer selection: ", computerSelection);
+// score tracker variables
+let playerScore = 0;
+let computerScore = 0;
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Rock, Paper, or Scissors?");
+    const lowerCasePlayerSelection = playerSelection.toLowerCase();
+    const computerSelection = getComputerChoice();
+    const winner = playRound(lowerCasePlayerSelection, computerSelection);
+
+    console.log("Player Selection: ", lowerCasePlayerSelection);
+    console.log("Computer Selection: ", computerSelection);
+    console.log("Winner: ", winner);
+    // make a new line
+    console.log("\n");
+
+    updateScore(winner);
+  }
+}
+
+// a function that determines final winner of game
+function whoWon(playerWins, computerWins) {
+  if (playerWins > computerWins) {
+    return "Congrats! You win!";
+  }
+  return "Sorry, you lose. Better luck next time!";
+}
+
+function updateScore(roundWinner) {
+  if (roundWinner === USER) {
+    playerScore++;
+  }
+  if (roundWinner === COMPUTER) {
+    computerScore++;
+  }
+}
+
+// Play the game
+game();
+
+const finalWinner = whoWon(playerScore, computerScore);
+
+console.log(finalWinner);
