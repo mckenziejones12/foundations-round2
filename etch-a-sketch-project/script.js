67f91ea4ac8742a34b520618c1d8e8781a4be6c5
McKenzie Jones <mckenzbeck12@gmail.com>
2023-01-30 07:51:32 -0700

    Add new features to grid

--- etch-a-sketch-project/script.js
+++ etch-a-sketch-project/script.js
@@ -1,44 +1,68 @@
+//elements created for html
 const body = document.querySelector("body");
 
 const newGridBtn = document.createElement("button");
 newGridBtn.textContent = "New Grid";
 newGridBtn.className = "btn";
-newGridBtn.id = "newGridBtn";
-newGridBtn.addEventListener("click", newGrid);
+newGridBtn.id = "newGrid";
 
 const clearBtn = document.createElement("button");
-clearBtn.textContent = "Clear";
+clearBtn.textContent = "Clear Grid";
 clearBtn.className = "btn";
+clearBtn.id = "clearGrid";
 
 const easGrid = document.createElement("div");
 easGrid.id = "easGrid";
 
-const gridSize = Number(prompt("How many squares per side to make new grid?"));
-
-function newGrid() {
-  console.log(gridSize);
-}
-
 //write function to display 16x16 grid
-function createGrid(gridSize) {
-  const rows = gridSize;
-  const columns = gridSize;
+function createGrid(num) {
+  const rows = num;
+  const columns = num;
   const gridArea = rows * columns;
-  easGrid.style.gridTemplateColumns = `repeat(${gridSize},auto)`;
+  easGrid.style.gridTemplateColumns = `repeat(${num},auto)`;
   for (let i = 0; i < gridArea; i++) {
     const cell = document.createElement("div");
+    cell.className = "cell";
     cell.addEventListener("mouseover", colorCell);
 
-    cell.className = "cell";
     easGrid.appendChild(cell);
   }
 }
-createGrid(gridSize);
+createGrid(16);
 
+//write function to display new grid when New Grid btn is clicked
+newGridBtn.addEventListener("click", newGrid);
+function newGrid() {
+  const newGridSize = prompt("How many squares per side to make new grid?");
+  if (newGridSize > 100) {
+    prompt("Sorry, too large of a number. Please pick a number 1-100.");
+  } else {
+    //remove old grid
+    const oldCells = document.querySelectorAll(".cell");
+    for (let i = 0; i < oldCells.length; i++) {
+      const singleCell = oldCells[i];
+      singleCell.remove();
+    }
+  }
+  //create  newGrid
+  createGrid(newGridSize);
+}
+
+clearBtn.addEventListener("click", clearGrid);
+function clearGrid() {
+  const singleCell = document.querySelectorAll(".cell");
+  for (let i = 0; i < singleCell.length; i++) {
+    const allCells = singleCell[i];
+    allCells.style.backgroundColor = "white";
+  }
+}
+
+//color each cell after mouseover
 function colorCell(event) {
   event.target.style.backgroundColor = "black";
 }
 
+//append elements to html
 body.appendChild(newGridBtn);
 body.appendChild(clearBtn);
 body.appendChild(easGrid);
