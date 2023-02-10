bd01b35515bb77f3e3b28a65a48ca282fde4929f
McKenzie Jones <mckenzbeck12@gmail.com>
2023-02-10 11:41:16 -0700

    Update functions

--- calculator-project/script.js
+++ calculator-project/script.js
@@ -3,47 +3,56 @@ let currentOperator = "";
 let currentOperand = "";
 let result = "";
 
-//grab all btns and display screens
+// grab all btns and display screens
 const numberBtns = document.querySelectorAll("[data-number]");
 const operatorBtns = document.querySelectorAll("[data-operator]");
 const deleteBtn = document.getElementById("deleteBtn");
-const decimalBtn = document.getElementById("decimal");
 const equalBtn = document.getElementById("equalBtn");
 const clearBtn = document.getElementById("clearBtn");
 const operationDisplay = document.getElementById("operationSection");
 const resultDisplay = document.getElementById("resultSection");
 
-//add event listeners
+// add event listeners
 numberBtns.forEach((button) =>
   button.addEventListener("click", () => chooseNumber(button.textContent))
 );
 operatorBtns.forEach((button) =>
   button.addEventListener("click", () => chooseOperator(button.textContent))
 );
+
 equalBtn.addEventListener("click", compute);
 clearBtn.addEventListener("click", clear);
 deleteBtn.addEventListener("click", removeSingleItem);
-decimalBtn.addEventListener("click", appendDecimal);
 
-//operator functions
+// operator functions
 const add = (num1, num2) => Number(num1) + Number(num2);
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
 function chooseNumber(number) {
-  currentOperand = number;
+  if (number === "." && currentOperand.includes(".")) {
+    return currentOperand.toString() + number.toString();
+  }
+  if (result !== "") {
+    currentOperand += number;
+    operationDisplay.textContent = `${previousOperand}${currentOperator}${currentOperand}`;
+    return;
+  }
+
+  currentOperand += number;
   operationDisplay.textContent = `${previousOperand}${currentOperator}${currentOperand}`;
 }
 function chooseOperator(operator) {
   currentOperator = operator;
 
   if (result !== "") {
+    currentOperand = "";
     operationDisplay.textContent = `${result}${currentOperator}`;
     return;
   }
   previousOperand = currentOperand;
-  currentOperator = operator;
+  currentOperand = "";
   operationDisplay.textContent = `${previousOperand}${currentOperator}`;
 }
 
@@ -69,7 +78,8 @@ function compute() {
   resultDisplay.textContent = result;
 }
 function appendDecimal() {
-  if (operationDisplay.textContent.includes(".")) {
+  if (operationDisplay.textContent.includes(".")) return;
+  {
     operationDisplay.textContent += ".";
   }
 }
@@ -87,9 +97,3 @@ function clear() {
   currentOperator = "";
   result = "";
 }
-
-console.log(add(2, 2)); //4
-console.log(subtract(2, 2)); //0
-console.log(multiply(2, 2)); //4
-console.log(divide(2, 2)); //1
-console.log(operate(2, "-", 2)); //0
